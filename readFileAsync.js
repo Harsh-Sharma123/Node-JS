49db4cfb3f57d0f9f16ec6a6945dc213efe59cba
Harsh Sharma <hs804506@gmail.com>
2024-12-14 22:34:37 +0530

    Write file async

--- readFileAsync.js
+++ readFileAsync.js
@@ -1,4 +1,12 @@
 const fs = require("fs");
 fs.readFile("./Files/input.txt", 'utf-8', (err, data) => {
     console.log(data);
-})
+    fs.writeFile("./Files/output2.txt", data, (err) => {
+        if(err){
+            console.log("Error while writing file : ", err);
+        }else{
+            console.log("File Written Successfully!!");
+        }
+    });
+}
+)
