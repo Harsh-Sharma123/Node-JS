d01f743f5ee6de316032f9ebd73a5493fa083831
Harsh Sharma <hs804506@gmail.com>
2024-12-17 23:46:59 +0530

    simple server config

--- createSimpleWebServer.js
+++ createSimpleWebServer.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log("A new req required !");
+    console.log("A new req received !");
+    res.end("Hello from Server !")
 });
 
 server.listen(4001, ()=>console.log("Server is Running at 4001"));
