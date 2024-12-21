9390dee34e60e285fcf3c195e50b490f72dcaebd
Harsh Sharma <hs804506@gmail.com>
2024-12-21 23:21:48 +0530

    req params in node js

--- createSimpleWebServer.js
+++ createSimpleWebServer.js
@@ -2,7 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log("A new req received !");
-    res.end("Hello from Server !")
+    // res.end("Hello from Server !")
+
+    console.log(req.url);
+
+    if(req.url === "/") res.end("Home Page !!");
+    else res.end("Not Home Page !!");
 });
 
 server.listen(4001, ()=>console.log("Server is Running at 4001"));
